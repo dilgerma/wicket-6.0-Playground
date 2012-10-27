899ac123c2efb9b277e542b51fb239653bccdfdc
Martin Dilger <martin@effectivetrainings.de>
2012-10-27 13:37:52 +0200

    initial state

---
+++ wicket-6-jquery/wicket-6-jquery/src/main/java/de/effectivetrainings/HomePage.java
@@ -0,0 +1,16 @@
+package de.effectivetrainings;
+
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.markup.head.JavaScriptContentHeaderItem;
+import org.apache.wicket.markup.html.internal.HtmlHeaderContainer;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.WebPage;
+
+import java.io.Serializable;
+
+public class HomePage extends WebPage {
+
+}
